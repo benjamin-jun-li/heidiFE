85dc1faa07d137896a9fe9097dda5daf8925323e
benjamin-jun-li <benjaminli060@gmail.com>
2024-06-04 16:12:41 +1000

    setup react, tailwind, shadcn

---
+++ .eslintrc.cjs
@@ -0,0 +1,39 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "prefer-arrow-functions"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "react/no-unstable-nested-components": ["error", { allowAsProps: true }],
+    "prefer-arrow-functions/prefer-arrow-functions": [
+      "warn",
+      {
+        classPropertiesAllowed: true,
+        disallowPrototype: true,
+        returnStyle: "unchanged",
+      },
+    ],
+    "arrow-body-style": "warn",
+    "prefer-arrow-callback": [
+      "warn",
+      {
+        allowNamedFunctions: true,
+      },
+    ],
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-one-expression-per-line": "off",
+    "react/prop-types": "off",
+    "indent": ["error", 2],
+  },
+};
---
+++ src/main.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  RouterProvider,
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+} from 'react-router-dom';
+import './index.css';
+import Home from './pages/home';
+import Header from './components/header';
+import Footer from './components/footer';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <React.Fragment>
+      <Route path="/" element={<Home />} />
+    </React.Fragment>
+  )
+);
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <Header />
+      <RouterProvider router={router} />
+      <Footer />
+    </React.StrictMode>
+  );
+}
